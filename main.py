89d01d76f7e6a55c160f92572074df3e4b80f19a
MartijnVDV1 <m.l.a.vandervoort@student.tudelft.nl>
2025-03-11 11:34:01 +0100

    c

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ to test out stuff
 '''
 
 import data_handler
-import data_importer
+import laser_tracker_data_importer
 
 
 
 # get cool data
-table = data_handler.handle_LT(data_importer._get_laser_tracker_raw(3))
+table = data_handler.handle_LT(laser_tracker_data_importer._get_laser_tracker_raw(3))
 
 
 print(table)
