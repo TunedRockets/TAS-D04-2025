1c086483fdb38de0ca2942d53f6582e09d6e19b3
seanmc <seanmcsean11@gmail.com>
2025-03-11 10:38:33 +0100

    LT

--- data_handler.py
+++ data_handler.py
@@ -11,31 +11,54 @@ import pandas as pd
 ################################################################################################################
 """Functions for Laser Tracker"""
 
-def function(x: list, y: list, z: list, time: list) -> pd.DataFrame:
-    rows = len(x)
+def handle_LT(time: list, x: list, y: list, z: list) -> pd.DataFrame:
+    rows = len(time)
     columns = 4
     shape = (rows, columns)
     np_array = np.empty(shape)
     pandas_table = pd.DataFrame(np_array)
 
-
     for i in range(len(x)):
-        pandas_table[i][0] = (x[i])
-        pandas_table[i][1] = (y[i])
-        pandas_table[i][2] = (z[i])
-        pandas_table[i][3] = (time[i])
+        pandas_table[i][0] = (time[i])
+        pandas_table[i][1] = (x[i])
+        pandas_table[i][2] = (y[i])
+        pandas_table[i][3] = (z[i])
 
     return pandas_table
 
 ################################################################################################################
 """Functions for Laser Line Scanner"""
 
+def handle_LLS(time: list, width: list, center: list) -> pd.DataFrame:
+    rows = len(time)
+    columns = 3
+    shape = (rows, columns)
+    np_array = np.empty(shape)
+    pandas_table = pd.DataFrame(np_array)
+
+    for i in range(len(time)):
+        pandas_table[i][0] = (time[i])
+        pandas_table[i][1] = (width[i])
+        pandas_table[i][2] = (center[i])
+
+    return pandas_table
 
 
 
 ################################################################################################################
 """Functions for Camera"""
 
+def handle_camera(time: list) -> pd.DataFrame:
+    rows = len(x)
+    columns = 1
+    shape = (rows, columns)
+    np_array = np.empty(shape)
+    pandas_table = pd.DataFrame(np_array)
+
+    for i in range(len(time)):
+        pandas_table[i][0] = time[i]
+
+    return pandas_table
 
 
 
