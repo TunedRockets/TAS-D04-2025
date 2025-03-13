750ecbed2cbbe1d91d8c83e7549d20f595cee62c
mcruz05 <mdegouveapinto@tudelft.nl>
2025-03-13 16:58:12 +0100

    Create data_statistics.py

---
+++ data_statistics.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib as plt
+
+#TODO: find the statistical values (mean, median, std, min, max) of the data
+#TODO: Create histograms of the four error types
+#TODO: create a 
+def data_statistics(data: pd.DataFrame):
