c4b5e60ec2fffe41a5390d4cd46e3b829aca75cc
diogoying <diogoying.co@gmail.com>
2025-03-13 15:14:43 +0100

    save to .csv

--- main.py
+++ main.py
@@ -8,9 +8,15 @@ import data_handler
 import laser_tracker_data_importer
 
 
+# Code to call the function multiple times
+def repeat_function(func, n):
+    for i in range(n):
+        func()
+repeat_function(handle_LT,3)
+
 
 # get cool data
-table = laser_tracker_data_importer._get_laser_tracker_raw(3)
+table = data_handler.handle_LT(laser_tracker_data_importer._get_laser_tracker_raw(3))
 
 
 print(table)
