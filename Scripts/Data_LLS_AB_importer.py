1340e39a9a5a422661244af4063979974af988a3
MartijnVDV1 <m.l.a.vandervoort@student.tudelft.nl>
2025-03-18 11:10:46 +0100

    LLS done now fr

--- Scripts/Data_LLS_AB_importer.py
+++ Scripts/Data_LLS_AB_importer.py
@@ -10,7 +10,7 @@ def LLS_exceltoarray(file_path):
     excel_data = pd.read_excel(file_path, sheet_name=None)
 
     # Create an array (list of DataFrames) to store data for each sheet
-    LLS_sheets_data = []
+    LLS_clean_sheets_data = []
 
     # Iterate through the sheets and store them into the array
     for sheet_name, sheet_df in excel_data.items():
@@ -18,20 +18,20 @@ def LLS_exceltoarray(file_path):
         if sheet_name.startswith("Run"):
             # Drop the first column (assuming the first column is always the first by index)
             LLS_sheet_df = sheet_df.iloc[:, 0:]
-            LLS_sheets_data.append(LLS_sheet_df)
+            LLS_clean_sheet = np.delete(LLS_sheet_df, [1, 2, 3, 4, 5, 6, 10], axis=1)
+            LLS_clean_sheets_data.append(LLS_clean_sheet)
 
     # Now `sheets_data` contains a list of DataFrames for all sheets starting with "Sheet",
     # excluding their first column.
     # You can access each sheet's data by index or iterate over the list.
     # Delete the columns that are not needed in each sheet
-    for i, sheet in enumerate(LLS_sheets_data):
-        LLS_clean_sheet = np.delete(sheet, [1, 2, 3, 4, 5, 6, 10], axis=1)
+  
         
     # Print the clean sheets
     #    print(f"clean sheet {i + 1}:")
     #    print(clean_sheet)
     
 
-    return LLS_clean_sheet
+    return LLS_clean_sheets_data
 
 print(LLS_exceltoarray(file_path))
