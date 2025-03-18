ca0a873de62ae440dd5e42ef988d9d0513a25546
cwalle123 <wcliftonjohn@gmail.com>
2025-03-18 10:26:23 +0100

    Update Data_LLS_AB_importer.py

--- Scripts/Data_LLS_AB_importer.py
+++ Scripts/Data_LLS_AB_importer.py
@@ -1,4 +1,4 @@
-"""
+
 import numpy as np
 import pandas as pd
 import glob
@@ -7,7 +7,7 @@ import os
 def get_directory_LLS_data(LLS_type, LLS_tow_number):
     """
     #Generates the directory for the LLS data files as a function of the LLS type and the tow number
-"""
+    """
     
     path_start = "Raw data\Data Sans Camera\LLS\Straight lines"
     path_extension = ".csv"
@@ -18,7 +18,7 @@ def get_directory_LLS_data(LLS_type, LLS_tow_number):
 def get_LLS_timestamps(line_id:int)->list:
     """
     #Gets the timestamps of the LLS data
-"""
+    """
 
     # get the path (currently fixed path, so don't move stuff around)
     path_start = "Raw data\Data Sans Camera\LLS\Straight lines"
@@ -40,7 +40,7 @@ def load_LLS_data(LLS_type, LLS_tow_number):
     """
     #For LLS type A: LLS_type = A_3060
     #For LLS type B: LLS_type = B_3010
-"""
+    """
     file_pattern = get_directory_LLS_data(LLS_type, LLS_tow_number) #Calls get_directory_LLS_data to generate the appropriate path
     #file_pattern = os.path.join(directory, f"LLS_{LLS_type}_data.csv")
     files = glob.glob(file_pattern)
@@ -73,11 +73,11 @@ def load_LLS_data(LLS_type, LLS_tow_number):
 def connect_LLS_timestamps_to_data(clean_LLS_time_data, all_coords):
     """
     #Stacks the timestamps column and the y and z coordinate columns next to each other and removes empty rows
-"""
+    """
     LLS_data_with_zeros = np.hstack((clean_LLS_time_data, all_coords)) #Stacking the columns
     LLS_data = np.array([row for row in LLS_data_with_zeros if not any(coord == 0 for coord in row[1:])], dtype=object)
     return LLS_data
-"""
+    
 
 # Example usage:
 # directory = "C:/Users/srott/PycharmProjects/TAS-D04-2025/Raw data/Data Sans Camera/LLS/Straight lines/1"
@@ -85,8 +85,6 @@ def connect_LLS_timestamps_to_data(clean_LLS_time_data, all_coords):
 # coords_B = load_laser_data(directory, "B_3010")
 # print(coords_A)
 # print(coords_B)
-
-import pandas as pd
 
 # Load the Excel file
 file_path = 'Data\Data Sans Camera\LLS\Straight lines\All runs widths\LLS_Width_Before_After.xlsx'  # Replace with your Excel file path
