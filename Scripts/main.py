afbf1f865562982fd08389af2b165116bbba211f
Johannes <johannes.nilson@outlook.com>
2025-03-27 16:44:06 +0100

    main

--- Scripts/main.py
+++ Scripts/main.py
@@ -7,6 +7,7 @@ to test out stuff
 import Handling_ALL_Functions
 
 # code to get all the data into .pkl
-# runs all the data and overwrites
-
-pass
+# runs all the data and overwrites everything.
+Handling_ALL_Functions.purge_cache() # argument is false so nobody accidentally does this (:
+Handling_ALL_Functions.create_cache()
+# will take some time to run
