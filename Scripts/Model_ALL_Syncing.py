1aa0930987fd1a8540982b2d2e17c392c26c5d2f
MartijnVDV1 <m.l.a.vandervoort@student.tudelft.nl>
2025-04-29 19:59:11 +0200

    hopefully the fix

--- Scripts/Model_ALL_Syncing.py
+++ Scripts/Model_ALL_Syncing.py
@@ -471,7 +471,7 @@ def _get_synced_data(tow:int, spacesynced:bool = False, overwrite:bool = False)-
         # do this by shifting the data in time, which makes it line up in space.
         timeshift_CAM = _space_time_shift(frame_LT["x"],frame_LT["time"],constants.TCP_CAM)
         timeshift_LLS_A = _space_time_shift(frame_LT["x"],frame_LT["time"],constants.TCP_LLS_A)
-        timeshift_LLS_B = _space_time_shift(frame_LT["x"],frame_LT["time"],constants.TCP_CAM)
+        timeshift_LLS_B = _space_time_shift(frame_LT["x"],frame_LT["time"],constants.TCP_LLS_B)
 
         # fix the CAM:
         #find closest point index in time:
