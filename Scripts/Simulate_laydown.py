8b4b80fe7d3c0ffe205c64351a48b68b1bd2976b
Johannes <johannes.nilson@outlook.com>
2025-04-29 08:47:49 +0200

    added functions to fill in

---
+++ Scripts/Simulate_laydown.py
@@ -0,0 +1,13 @@
+
+
+
+def simulate_laydown():
+    '''Simulates the generation of a tow of data'''
+
+    raise NotImplementedError
+
+
+def gap_overlaps():
+    '''evaluates the gap/overlap of a tow of data'''
+
+    raise NotImplementedError
