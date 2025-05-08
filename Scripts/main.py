e5bba29db13d90a1a6fc4fc43ea862c95fb39b26
cwalle123 <wcliftonjohn@gmail.com>
2025-05-08 15:00:13 +0200

    update main

--- Scripts/main.py
+++ Scripts/main.py
@@ -5,12 +5,18 @@ to test out stuff
 '''
 import time
 import Handling_ALL_Functions
+from Model_ALL_ConsecutiveModeler import tow_visualizer
 
 # code to get all the data into .pkl
 # runs all the data and overwrites everything.
 start = time.time()
-Handling_ALL_Functions.purge_cache() # argument is false so nobody accidentally does this (:
-Handling_ALL_Functions.create_cache()
-Handling_ALL_Functions.create_processed_cache()
+# Handling_ALL_Functions.purge_cache() # argument is false so nobody accidentally does this (:
+# Handling_ALL_Functions.create_cache()
+# Handling_ALL_Functions.create_processed_cache()
+
+# run tow visualizer
+tow_visualizer()
+
+
 print(f"elapsed time: {time.time() - start} (s)")
 # will take some time to run
