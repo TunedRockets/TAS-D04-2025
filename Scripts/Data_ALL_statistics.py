c50d6800b5f7e5284962f9c3199fa536c97e2282
mcruz05 <mdegouveapinto@tudelft.nl>
2025-05-08 16:00:58 +0200

    Update Data_ALL_statistics.py

--- Scripts/Data_ALL_statistics.py
+++ Scripts/Data_ALL_statistics.py
@@ -346,12 +346,12 @@ def main():
         df,
         title="Sensor Error Histograms (ALL TOWS)",
         bin_widths=[0.005, 0.005, 0.005, 0.03],
-        run = True)
+        run = False)
 
     plot_LLSA_vs_LLSB(df,
         title="Error LLS A vs. Error LLS B (ALL TOWS)",
         bin_widths=[0.005, 0.005],
-        run = False)
+        run = True)
 
 if __name__ == "__main__":
     main()
